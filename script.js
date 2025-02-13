81a8bbb4e1cbcc2099bf1b350efd88c81f997591
Ian Paulo M. Gilera <ianpaulomgilera@iskolarngbayan.pup.edu.ph>
2025-02-13 15:43:06 +0800

    changed hearts to flowers

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ function preventClose() {
 function createHearts() {
     const heart = document.createElement("div");
     heart.classList.add("heart");
-    heart.innerHTML = "❤️";
+    heart.innerHTML = "💐";
     heart.style.left = `${Math.random() * 100}vw`;
     heart.style.animationDuration = `${Math.random() * 3 + 2}s`;
     document.body.appendChild(heart);
